3437454f5600961314f1d72f8ae57591761b34d3
PSUGitTest17vv2 <PSUGitTest17vv2@mail.ru>
2021-09-16 05:44:11 +0300

    add class view c#

--- SDT/View.h
+++ SDT/View.h
@@ -1,1 +1,17 @@
 #pragma once
+#ifndef VIEW_H_
+#define VIEW_H_
+
+#include <string>
+
+class StudentView
+{
+    public void printStudentDetails(String studentName, String studentRollNo)
+    {
+        System.out.println("Student: ");
+        System.out.println("Name: " + studentName);
+        System.out.println("Roll No: " + studentRollNo);
+    }
+}
+
+#endif // !VIEW_H_
