19431b3dd3960059a514a419cf193a6eaba249d9
PSUGitTest17vv2 <PSUGitTest17vv2@mail.ru>
2021-09-16 05:40:57 +0300

    add class model c#

--- SDT/Model.h
+++ SDT/Model.h
@@ -1,1 +1,33 @@
 #pragma once
+#ifndef MODEL_H_
+#define MODEL_H_
+
+#include <string>
+
+class Student
+{
+    private String rollNo;
+    private String name;
+
+    public String getRollNo()
+    {
+        return rollNo;
+    }
+
+    public void setRollNo(String rollNo)
+    {
+        this.rollNo = rollNo;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
+
+#endif // !MODEL_H_
