81b189b5c3f89b9cedac64283dd20b2f056d95f9
PSUGitTest17vv2 <PSUGitTest17vv2@mail.ru>
2021-09-16 06:26:24 +0300

    amendment

--- SDT/MVC.cpp
+++ SDT/MVC.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <string>
 
 #include "Controller.h"
 #include "Model.h"
 #include "View.h"
 
+inline void retriveStudentFromDatabase(Model::Student& student,
+									     std::string name,
+									     std::string rollno)
+{
+	student.setName(name);
+	student.setRollNo(rollno);
+}
+
 int main()
 {
+
 	return 0;
 }
