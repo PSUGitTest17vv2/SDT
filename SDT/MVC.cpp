68d569d526964f85041b13f86e7e3564aaff08e8
PSUGitTest17vv2 <PSUGitTest17vv2@mail.ru>
2021-09-16 06:54:58 +0300

    modify file MVC

--- SDT/MVC.cpp
+++ SDT/MVC.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <string>
 
 #include "Controller.h"
 #include "Model.h"
 #include "View.h"
 
+inline void retriveStudentFromDatabase(model::Student& student,
+										 std::string name,
+										 std::string rollno)
+{
+	student.setName(name);
+	student.setRollNo(rollno);
+}
+
 int main()
 {
+	model::Student model;
+	retriveStudentFromDatabase(model, "Mark Abramov", "15UCS157");
+
+	view::Student view;
+	view.printStudentDetails(model.getName(), model.getRollNo());
+
 	return 0;
 }
